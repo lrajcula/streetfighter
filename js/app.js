af53e1c042f15cc10db4eeac2408921741f123e7
lrajcula <lrajcula02@gmail.com>
2015-11-04 21:46:32 -0500

    animate Ryu and add smedia

--- js/app.js
+++ js/app.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	});
 	$('.ryu').mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show(); 
-		$('.hadouken').show();
-		//animate hadouken to the right of the screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
@@ -24,4 +31,10 @@ $(document).ready(function() {
 	});
 
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
 
